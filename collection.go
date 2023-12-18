79f2fc606fbcc8e82c532db1e14e9c03f0e92d24
pluja <github_pluja@r3d.red>
2023-12-18 11:50:33 +0100

    add support for OneWithParams

--- collection.go
+++ collection.go
@@ -64,3 +64,37 @@ func (c Collection[T]) One(id string) (T, error) {
 	}
 	return response, nil
 }
+
+// Get one record with params (only fields and expand supported)
+func (c Collection[T]) OneWithParams(id string, params ParamsList) (T, error) {
+	var response T
+
+	if err := c.Authorize(); err != nil {
+		return response, err
+	}
+
+	request := c.client.R().
+		SetHeader("Content-Type", "application/json").
+		SetPathParam("collection", c.Name).
+		SetPathParam("id", id).
+		SetQueryParam("fields", params.Fields).
+		SetQueryParam("expand", params.Expand)
+
+	resp, err := request.Get(c.url + "/api/collections/{collection}/records/{id}")
+	if err != nil {
+		return response, fmt.Errorf("[one] can't send update request to pocketbase, err %w", err)
+	}
+
+	if resp.IsError() {
+		return response, fmt.Errorf("[one] pocketbase returned status: %d, msg: %s, err %w",
+			resp.StatusCode(),
+			resp.String(),
+			ErrInvalidResponse,
+		)
+	}
+
+	if err := json.Unmarshal(resp.Body(), &response); err != nil {
+		return response, fmt.Errorf("[one] can't unmarshal response, err %w", err)
+	}
+	return response, nil
+}
